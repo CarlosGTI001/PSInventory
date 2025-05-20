768d93cd0e9c670a36fcddcec762d609bcb88fdc
Camaras <Camaras@DESKTOP-SINIFVB>
2025-05-20 16:36:41 -0400

    Agregar archivos de proyecto.

---
+++ PSInventory/Modelos/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSInventory.Modelos
+{
+    public class Usuario
+    {
+        [Key]
+        [Required]
+        public string UsuarioId { get; set; }
+
+        [Required]
+        public string UsuarioNombre { get; set; }
+
+        [Required]
+        public string UsuarioPassword { get; set; }
+
+        [Required]
+        public string UsuarioEmail { get; set; }
+
+        [Required]
+        public string Rol { get; set; }
+    }
+}
